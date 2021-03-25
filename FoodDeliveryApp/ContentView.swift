6f1fad4a3776683d248cf372cdc2ff86759acb27
Madhav Jaisankar <49799271+MillionLemur767@users.noreply.github.com>
2021-03-25 17:59:34 -0400

    Initial Commit

---
+++ FoodDeliveryApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodDeliveryApp
+//
+//  Created by Madhav Jaisankar on 2021-03-25.
+//  Copyright © 2021 Madhav Jaisankar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
