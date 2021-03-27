94eb2a3a4a7adef3b979f10bcad9c31b93284963
Madhav Jaisankar <49799271+MillionLemur767@users.noreply.github.com>
2021-03-27 00:25:06 -0400

    Created two textfields for username and password in loginScreen

--- FoodDeliveryApp/ContentView.swift
+++ FoodDeliveryApp/ContentView.swift
@@ -15,12 +15,36 @@ struct ContentView: View {
     var body: some View {
 //        Text("food delivery app")
         ZStack {
-            Color.blue.opacity(0.2)
-            VStack{
-                TextField("Username", text: $username).textFieldStyle(RoundedBorderTextFieldStyle())
-                TextField("Password", text: $password).textFieldStyle(RoundedBorderTextFieldStyle())
-            }
+            Color.pink.opacity(0.85)
+            VStack(alignment: .leading){
+                Text("Enter your username")
+                    .foregroundColor(Color.white)
+                    .fontWeight(.bold)
+                HStack{
+                    TextField("Username", text: $username).textFieldStyle(OvalTextFieldStyle())
+                    
+                }.padding(.bottom, 20)
+                Text("Enter your password")
+                    .foregroundColor(Color.white)
+                    .fontWeight(.bold)
+                HStack{
+                    TextField("Password", text: $password).textFieldStyle(OvalTextFieldStyle())
+                    
+                }
+            }.padding()
         }
+    }
+}
+
+struct OvalTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding(10)
+            .background(LinearGradient(gradient: Gradient(colors: [Color.white, Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing))
+            .cornerRadius(10)
+            .overlay(RoundedRectangle(cornerRadius: 10)
+            .stroke(Color.green, lineWidth: 3))
+            .shadow(color: .gray, radius: 2)
     }
 }
 
